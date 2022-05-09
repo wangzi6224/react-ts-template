e64ee1be8ca6b381cd57b9ae5cf91480524d153e
wangzi6224 <wangzi6224@gmail.com>
2022-05-09 17:48:20 +0800

    feature: 提交测试

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import './index.less';
 
 const App: React.FC = () => {
+    1 ? 2 : 3
     return (
         <div className="app">
             Hello World
         </div>
     )
-};
+}
 
-export default App;
+export default App
 
